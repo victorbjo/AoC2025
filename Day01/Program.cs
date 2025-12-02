502f4ca59e67795fef13f482abfe735b8cd28404
VictorBjo <victor.bjoerholm@gmail.com>
2025-12-02 16:17:42 +0100

    Initial commit

---
+++ Day01/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read all lines from the input file
+            string[] lines = File.ReadAllLines("input.txt");
+            int part1Result = Part1(lines);
+            Console.WriteLine(part1Result);
+            
+
+            int part2Result = Part2(lines);
+            Console.WriteLine(part2Result);
+        }
+        static int Part1(string[] lines) {
+            // Print the number of lines
+            Console.WriteLine($"Number of lines: {lines.Length}");
+            int sum = 50;
+            int metaSum = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].Contains("L")){
+                    sum -= int.Parse(lines[i].Substring(1, lines[i].Length-1));
+                }
+                else {
+                    sum += int.Parse(lines[i].Substring(1, lines[i].Length-1));
+                }
+
+                if (sum < 0 && Math.Abs(sum)%100 != 0)
+                {
+                    sum = Math.Abs(sum) % 100;
+                    sum = 100 - sum;
+                }
+                else if (Math.Abs(sum) >= 100)
+                {
+                    sum = sum%100;
+                }
+                if (sum == 0){
+                    metaSum += 1;
+                }
+            }
+            return metaSum;
+        }
+        
+
+        static int Part2(string[] lines) {
+             // Print the number of lines
+            Console.WriteLine($"Number of lines: {lines.Length}");
+            int sum = 50;
+            int metaSum = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].Contains("L"))
+                {
+                    if (sum == 0) //If going left from 0, decrease metaSum by 1, as any negative sum will later be counted as having passed 0 at least once
+                    {
+                        metaSum -= 1;
+                    }
+                    sum -= int.Parse(lines[i].Substring(1, lines[i].Length - 1));
+                }
+                else
+                {
+                    sum += int.Parse(lines[i].Substring(1, lines[i].Length - 1));
+                }
+                if (sum == 0)
+                {
+                    metaSum += 1;
+                }
+                if (sum < 0)
+                {
+                    int temp = Math.Abs(sum) / 100;
+                    metaSum += temp + 1;
+                    sum = Math.Abs(sum) % 100;
+                    sum = 100 - sum;
+                    if (sum == 100)
+                    { 
+                        sum = 0;
+                    }
+                    
+                }
+                else if (sum >= 100)
+                {
+                    int temp = sum / 100;
+                    metaSum += temp;
+                    sum = sum % 100;
+                }
+                if (sum == 0)
+                {
+                    metaSum += 0;
+                }
+                //Print current sum and metaSum
+                Console.WriteLine($"Current sum: {sum}, metaSum: {metaSum}, movement {lines[i]}");
+            }
+            return metaSum;
+        }
+    }
+}
---
+++ Day02/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read all lines from the input file
+            string lines = File.ReadAllText("input.txt");
+            //long part1_solution = part1(lines);
+            long part2_solution = part2(lines);
+            //Console.WriteLine($"Part 1 Solution: {part1_solution}");
+            Console.WriteLine($"Part 2 Solution: {part2_solution}");
+            Console.WriteLine($"Part 2 Solution: {check_validity_part2(999)}");
+        }
+
+        static long part1(string lines)
+        {
+            string[] ranges = lines.Split(",");
+            long sum = 0;
+            for (int i = 0; i < ranges.Length; i++)
+            {
+                //sum = 0;
+                string[] range_temp = ranges[i].Split("-");
+                long range_lower = long.Parse(range_temp[0]);
+                long range_upper = long.Parse(range_temp[1]);
+
+                for (long j = range_lower; j <= range_upper; j++)
+                {
+                    //bool value = check_validity(j);
+                    sum += check_validity(j) ? j : 0;
+                }
+                //Console.WriteLine($"range_lower, range_upper: {range_lower},{range_upper}, {sum}");
+            }
+            return sum;
+        }
+        static bool check_validity(long ID)
+            {
+                string id_string = ID.ToString();
+                if (id_string.Length % 2 != 0) return false;
+                for (int i = 0; i < id_string.Length / 2; i++)
+                {
+                    if (id_string[i] != id_string[id_string.Length/2 + i]) {
+                        return false;
+                    }
+                }
+            return true;
+            }
+        static long part2(string lines)
+        {
+            string[] ranges = lines.Split(",");
+            long sum = 0;
+            for (int i = 0; i < ranges.Length; i++)
+            {
+                string[] range_temp = ranges[i].Split("-");
+                long range_lower = long.Parse(range_temp[0]);
+                long range_upper = long.Parse(range_temp[1]);
+
+                for (long j = range_lower; j <= range_upper; j++)
+                {
+                    sum += check_validity_part2(j) ? j : 0;
+                }
+            }
+            return sum;
+        }
+        static bool check_validity_part2(long ID)
+        {
+            string id_string = ID.ToString();
+            for (int i = 1; i <= id_string.Length; i++)
+            {
+                if (id_string.Length % (i+1) != 0)
+                {
+                    continue;
+                }
+                int subArray_size = id_string.Length / (i+1);
+                var subArrays = id_string
+                    .Select((value, index) => new { value, index })
+                    .GroupBy(item => item.index / subArray_size)
+                    .Select(group => group.Select(item => item.value).ToArray())
+                    .ToArray();
+                bool allEqual = subArrays.All(sub => sub.SequenceEqual(subArrays[0]));
+
+                if (allEqual)
+                {
+                    Console.WriteLine($"ID: {ID}");
+                    return allEqual;
+                }
+
+            }
+            return false;
+        }
+        }
+    }
