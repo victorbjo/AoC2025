0cfe5cf378e187cb1e303c73790f7fdade30880f
VictorBjo <victor.bjoerholm@gmail.com>
2025-12-08 21:46:20 +0100

    Even worse part 2

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -70,7 +70,7 @@ namespace Day08
             List<List<JunctionBox>> listOfUnions = new List<List<JunctionBox>>();
             for (int i = 0; i < 1000; i++)
             {
-                
+
                 List<int> listToMerge = new List<int>();
                 for (int j = 0; j < listOfUnions.Count(); j++)
                 {
@@ -110,20 +110,92 @@ namespace Day08
                     // Add the merged, deduplicated union
                     listOfUnions.Add(mergedUnion);
                 }
-                //Console.WriteLine($"{distanceList[i].Box1},{distanceList[i].Box2}, circuits : {listOfUnions.Count()}, Dist : {distanceList[i].Box1.DistanceTo(distanceList[i].Box2)}");
             }
             listOfUnions = listOfUnions.OrderByDescending(union => union.Count).ToList();
-            //Console.WriteLine($"Circuits: {listOfUnions[0].Count()}, {listOfUnions[1].Count()},{listOfUnions[2].Count()},{listOfUnions[3].Count()}{listOfUnions[0][0]}, {listOfUnions[0][1]},{listOfUnions[0][2]} ");
-            // Console.WriteLine(listOfUnions[0].Count());
+
             long sum = listOfUnions[0].Count() * listOfUnions[1].Count() * listOfUnions[2].Count();
-            //Console.WriteLine(distanceList.Count());
-            //Console.WriteLine($"{distanceList[0].Dist}, {distanceList[0].Box1}, {distanceList[0].Box2}");
-            //Console.WriteLine($"{distanceList[1].Dist}, {distanceList[1].Box1}, {distanceList[1].Box2}");
             return sum;
-            //return 0;
+
         }
         static long Part2(string[] lines)
         {
+
+            List<JunctionBox> jList = new List<JunctionBox>();
+            foreach (string line in lines)
+            {
+                string[] coordinates3D = line.Split(",");
+                JunctionBox point3D = new JunctionBox(int.Parse(coordinates3D[0]), int.Parse(coordinates3D[1]), int.Parse(coordinates3D[2]));
+                jList.Add(point3D);
+            }
+            List<(double Dist, JunctionBox Box1, JunctionBox Box2)> distanceList = new List<(double, JunctionBox, JunctionBox)>();
+            for (int i = 0; i < jList.Count(); i++)
+            {
+                for (int j = i; j < jList.Count(); j++)
+                {
+                    if (j == i)
+                    {
+                        continue;
+                    }
+                    var distance = (Dist: jList[i].DistanceTo(jList[j]), Box1: jList[i], Box2: jList[j]);
+                    distanceList.Add(distance);
+                }
+            }
+            distanceList = distanceList
+                .OrderBy(item => item.Dist)
+                .ToList();
+
+            List<List<JunctionBox>> listOfUnions = new List<List<JunctionBox>>();
+            for (int i = 0; i < 100000; i++)
+            {
+                List<int> listToMerge = new List<int>();
+                for (int j = 0; j < listOfUnions.Count(); j++)
+                {
+                    if (listOfUnions[j].Contains(distanceList[i].Box1) || listOfUnions[j].Contains(distanceList[i].Box2))
+                    {
+                        if (!listOfUnions[j].Contains(distanceList[i].Box1))
+                            listOfUnions[j].Add(distanceList[i].Box1);
+                        if (!listOfUnions[j].Contains(distanceList[i].Box2))
+                            listOfUnions[j].Add(distanceList[i].Box2);
+                        listToMerge.Add(j);
+                    }
+                }
+                //If no listToMerge is 0, then no lists containing either junctionboxes has been found and a new union will be added
+                if (listToMerge.Count() == 0)
+                {
+                    List<JunctionBox> tempList = new List<JunctionBox>();
+                    tempList.Add(distanceList[i].Box1);
+                    tempList.Add(distanceList[i].Box2);
+                    listOfUnions.Add(tempList);
+                }
+                else if (listToMerge.Count() > 1)
+                {
+                    List<JunctionBox> mergedUnion = new List<JunctionBox>();
+                    foreach (int index in listToMerge)
+                    {
+                        mergedUnion.AddRange(listOfUnions[index]);
+                    }
+                    // Remove duplicates
+                    mergedUnion = mergedUnion.Distinct().ToList();
+
+                    // Remove old unions (descending order)
+                    listToMerge.Sort((a, b) => b.CompareTo(a));
+                    foreach (int index in listToMerge)
+                    {
+                        listOfUnions.RemoveAt(index);
+                    }
+                    // Add the merged, deduplicated union
+                    listOfUnions.Add(mergedUnion);
+                }
+                //Console.WriteLine(listOfUnions.Count());
+                if (listOfUnions[0].Count() == 1000)
+                {
+                    long dist = (long)distanceList[i].Box1.X * (long)distanceList[i].Box2.X;
+                    return dist;
+                }
+            }
+            listOfUnions = listOfUnions.OrderByDescending(union => union.Count).ToList();
+
+            //long sum = listOfUnions[0].Count() * listOfUnions[1].Count() * listOfUnions[2].Count();
             return 0;
         }
     }
